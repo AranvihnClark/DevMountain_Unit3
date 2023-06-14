64ba763af0c50716abd0285c9deffd47ec7d11b8
Aranvihn Clark <aranvihn.clark@gmail.com>
2023-06-14 15:21:28 -0400

    Update - Project3 Complete

--- java-proj-3/starter/src/main/java/com/javaunit3/springmvc/MovieController.java
+++ java-proj-3/starter/src/main/java/com/javaunit3/springmvc/MovieController.java
@@ -1,5 +1,8 @@
 package com.javaunit3.springmvc;
 
+import com.javaunit3.springmvc.model.MovieEntity;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,6 +15,9 @@ public class MovieController {
 
     @Autowired
     BestMovieService bestMovieService;
+
+    @Autowired
+    private SessionFactory sessionFactory;
 
     @RequestMapping("/")
     public String getIndexPage() {
@@ -29,5 +35,45 @@ public class MovieController {
         String movieTitle = request.getParameter("movieTitle");
         model.addAttribute("BestMovieVote", movieTitle);
         return "voteForBestMovie";
+    }
+
+    @RequestMapping("/addMovieForm")
+    public String addMovieForm() {
+        return "addMovie";
+    }
+
+    @RequestMapping("/addMovie")
+    public String addMovie(HttpServletRequest request)
+    {
+        // Assigns local variables with strings we inputted from the html.
+        // Though I feel it's not really necessary because we can just push this straight into our movie entity below and save on memory,
+        // But I guess it wouldn't look as nice...
+        String movieTitle = request.getParameter("movieTitle");
+        String maturityRating = request.getParameter("maturityRating");
+        String genre = request.getParameter("genre");
+
+        // A new MovieEntity is created (which just means a new entry in our table is created).
+        MovieEntity movieEntity = new MovieEntity();
+
+        // I wonder why we do it like below to set the column info in our table and why we don't just create a constructor.
+        // But, thinking now, I guess if it gets bigger it would be an ugly constructor call.
+        movieEntity.setTitle(movieTitle);
+        movieEntity.setMaturityRating(maturityRating);
+        movieEntity.setGenre(genre);
+
+        // Creates a new session factory to 'Update' our persistent table...
+        // Because sessionFactory was autowired, I believe it will automatically grab the 'movies' table -- if I'm understanding this right.
+        // Since our only SessionFactory 'bean' was for MovieEntity, I think I understood this correctly.
+        // The '.getCurrentSession' will automatically decide what table we need based on the 'current' session or location we are at in the html.
+        Session session = sessionFactory.getCurrentSession();
+
+        // Starts the updating process (though I guess it would be more accurate to say this starts the transfer process)
+        session.beginTransaction();
+
+        // We save our new row to the table.
+        session.save(movieEntity);
+        session.getTransaction().commit();
+
+        return "addMovie";
     }
 }
---
+++ java-proj-3/starter/src/main/java/com/javaunit3/springmvc/model/MovieEntity.java
@@ -0,0 +1,58 @@
+package com.javaunit3.springmvc.model;
+
+import javax.persistence.*;
+
+// Table creation
+@Entity
+@Table(name = "movies")
+public class MovieEntity {
+
+    // Column creation
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Column (name = "movie_id")
+    private Integer id;
+
+    @Column (name = "title")
+    private String title;
+
+    @Column (name = "maturity_rating")
+    private String maturityRating;
+
+    @Column (name = "genre")
+    private String genre;
+
+    // Getters and Setters;
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getMaturityRating() {
+        return maturityRating;
+    }
+
+    public void setMaturityRating(String maturityRating) {
+        this.maturityRating = maturityRating;
+    }
+
+    public String getGenre() {
+        return this.genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+}
